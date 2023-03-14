2bcea69870cc294142f5d0f46d6ee04bfae9f08d
Daniela Moreno <danielamoreno699@gmail.com>
2023-03-13 20:18:16 -0500

    Add cards as objects in the main.js

---
+++ main.js
@@ -0,0 +1,40 @@
+const cards = [
+  {
+    id: 0,
+    img: '',
+    guestName: 'Yochai Bencher',
+    professionalBackground: 'Berkman profesor of Enterpreneuial Legal Studies',
+    shortResume: 'Benkler studies commons-based peer production and published his seminal book The Wealth of Networks in 2006',
+
+  },
+  {
+    id: 1,
+    img: '',
+    guestName: 'Yochai Bencher',
+    professionalBackground: 'Berkman profesor of Enterpreneuial Legal Studies',
+    shortResume: 'Benkler studies commons-based peer production and published his seminal book The Wealth of Networks in 2006',
+
+  },
+  {
+    id:2,
+    img: '',
+    guestName: 'Yochai Bencher',
+    professionalBackground: 'Berkman profesor of Enterpreneuial Legal Studies',
+    shortResume: 'Benkler studies commons-based peer production and published his seminal book The Wealth of Networks in 2006',
+
+  },
+  {
+    id: 3,
+    img: '',
+    guestName: 'Yochai Bencher',
+    professionalBackground: 'Berkman profesor of Enterpreneuial Legal Studies',
+    shortResume: 'Benkler studies commons-based peer production and published his seminal book The Wealth of Networks in 2006',
+  },
+  {
+    id: 4,
+    img: '',
+    guestName: 'Yochai Bencher',
+    professionalBackground: 'Berkman profesor of Enterpreneuial Legal Studies',
+    shortResume: 'Benkler studies commons-based peer production and published his seminal book The Wealth of Networks in 2006',
+  },
+];
